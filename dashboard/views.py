9d47b74fd536107ce9e1566c4121e49fa75ac6f3
mungai <mungai.mbugua1409@gmail.com>
2024-08-07 16:22:45 +0300

    work on functionalities of notes section

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from . forms import *
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request,'dashboard/home.html')
+
+def notes(request):
+    if request.method == "POST":
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request,f"Notes added from {request.user.username} successfully!")
+    else:
+        form = NotesForm()
+        notes = Notes.objects.filter(user=request.user)
+        context = {'notes':notes,'form':form}
+        return render(request,'dashboard/notes.html',context)            
+    
+def delete_note(request,pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")    
